b75be4442c849b8ad6cc2a877cbe5c07c654316b
hyjoong <maplesosser@naver.com>
2021-12-24 23:50:20 +0900

    feat: ListView & 반복문 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());  // start App
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  var tmp =1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            child: Text(tmp.toString()),
+            onPressed: (){
+              print(tmp);
+              tmp++;
+            },
+          ),
+          appBar:AppBar(),
+          body:ListView.builder(
+            itemCount:3,
+            itemBuilder: (c,i){
+              return ListTile(
+                leading: Image.asset('hams.png'),
+                title: Text('김이름'),
+              );
+            }
+        )
+      )
+    );
+  }
+}
+
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      child: Text('안녕'),
+    );
   }
 }
